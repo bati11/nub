0ee4c45999e9e6e5cdd876292c6dbf84c1d0532c
bati11 <mail.bati11@gmail.com>
2017-01-22 16:49:44 +0900

    Add comments

--- src/main/java/com/github/kmizu/nub/Main.java
+++ src/main/java/com/github/kmizu/nub/Main.java
@@ -8,9 +8,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException{
         if(args.length == 0) {
+            // streamは入力（nubプログラムのソースコード）
             CommonTokenStream stream = Streams.streamFrom("def printRange(from, to) { let i = from; while(i < to) { println(i); i = i + 1; } } printRange(1, 10); // Loop");
+
+            // NubParserで自動生成されたもの
+            // 字句解析 + 構文解析をして抽象構文木を返す
+            // programは抽象構文木
             AstNode.ExpressionList program = new NubParser(stream).program().e;
+
+            // evaluatorが処理系
             Evaluator evaluator = new Evaluator();
+
+            // 処理系でnubプログラムを実行
             evaluator.evaluate(program);
         } else {
             String fileName = args[0];
